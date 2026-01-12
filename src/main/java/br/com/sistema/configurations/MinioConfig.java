b5d2abf39e0a5e0512968e2ebf87141656ff0ead
Cesar Augusto <cesar.augusto.rj1@gmail.com>
2026-01-11 22:55:22 -0300

    chore(minio): configura client S3 e inicialização automática do bucket

--- src/main/java/br/com/sistema/configurations/MinioConfig.java
+++ src/main/java/br/com/sistema/configurations/MinioConfig.java
@@ -6,15 +6,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.S3Configuration;
-import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
-import software.amazon.awssdk.services.s3.model.HeadBucketRequest;
 
 @Configuration
 @Slf4j
@@ -32,53 +29,22 @@ public class MinioConfig {
     @Value("${minio.region}")
     private String region;
 
-    @Value("${minio.bucket-name}")
-    private String bucketName;
-
     @Bean
     public S3Client s3Client() {
-        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
+        AwsBasicCredentials credentials =
+                AwsBasicCredentials.create(accessKey, secretKey);
 
         return S3Client.builder()
                 .endpointOverride(URI.create(endpoint))
-                .credentialsProvider(StaticCredentialsProvider.create(credentials))
+                .credentialsProvider(
+                        StaticCredentialsProvider.create(credentials)
+                )
                 .region(Region.of(region))
-                .serviceConfiguration(S3Configuration.builder()
-                        .pathStyleAccessEnabled(true)
-                        .build())
+                .serviceConfiguration(
+                        S3Configuration.builder()
+                                .pathStyleAccessEnabled(true)
+                                .build()
+                )
                 .build();
-    }
-
-    @PostConstruct
-    public void init() {
-        log.info("MinIO configurado - Endpoint: {}", endpoint);
-        log.info("MinIO - Bucket: {}", bucketName);
-        createBucketIfNotExists();
-    }
-
-    private void createBucketIfNotExists() {
-        try {
-            S3Client client = s3Client();
-            HeadBucketRequest headBucketRequest = HeadBucketRequest.builder()
-                    .bucket(bucketName)
-                    .build();
-            
-            client.headBucket(headBucketRequest);
-            log.info("Bucket '{}' já existe", bucketName);
-            
-        } catch (Exception e) {
-            try {
-                S3Client client = s3Client();
-                CreateBucketRequest createBucketRequest = CreateBucketRequest.builder()
-                        .bucket(bucketName)
-                        .build();
-                
-                client.createBucket(createBucketRequest);
-                log.info("Bucket '{}' criado com sucesso", bucketName);
-                
-            } catch (Exception ex) {
-                log.error("Erro ao criar bucket '{}': {}", bucketName, ex.getMessage());
-            }
-        }
     }
 }
